f1b1e54fc4b852616492d21eb46a9dab2cde7d12
Michael Fink <Michael-Fink@bittner-krull.de>
2025-06-25 15:07:58 +0200

    added sample sandbox apps for Android, iOS and UWP, just like in .NET MAUI

---
+++ Xamarin.Forms.Core.Sample.Sandbox/Xamarin.Forms.Core.Sample.Sandbox/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin.Forms.Core.Sample.Sandbox
+{
+	public partial class MainPage : ContentPage
+	{
+		// Place your sample sandbox page code here
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Xamarin.Forms.Core.Sample.Sandbox/Xamarin.Forms.Core.Sample.Sandbox/MainPageViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Xamarin.Forms.Core.Sample.Sandbox
+{
+	class MainPageViewModel
+	{
+		// Place your sample sandbox view model code here
+	}
+}
